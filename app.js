1621ace58cbb8ac57c69a5a964265d37494c25c4
Thiru2115 <138439214+Thiru2115@users.noreply.github.com>
2024-02-09 01:32:40 +0530

    Update app.js updated all app code

--- app.js
+++ app.js
@@ -2,30 +2,38 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const userRouter = require("./router/userRoutes");
-app.use(express.json());
-app.use(cors({
-  origin: 'https://frontendzomoto-nqv4d9oq7-thirumoothis-projects.vercel.app/' 
-}));
+const mongoose = require("mongoose");
+const route = require("./router");
 
 const port = 8900;
 
+// Use CORS middleware to allow requests from your Vercel domain
+app.use(cors({
+  origin: "https://frontendzomoto.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"], // specify the allowed HTTP methods
+}));
+
+app.use(express.json());
+
+// Use your user router
 app.use("/", userRouter);
 
-const mongoose = require("mongoose");
+// Connect to MongoDB
 mongoose
   .connect(
     "mongodb+srv://Thiru2115:Thiru11222333344444@cluster0.woqafsk.mongodb.net/",
-    /* { useNewUrlParser: true, useUnifiedTopology: true } */
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
     console.log("Database Connected");
+    // Start the server
     app.listen(port, () => {
       console.log("App Running");
     });
   })
   .catch((err) => {
-    console.log(err);
+    console.error("Database connection error:", err);
   });
 
-const route = require("./router");
+// Use your other routes
 app.use("/", route);
